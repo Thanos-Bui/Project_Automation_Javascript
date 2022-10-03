bbed71e76e27e47258e897b686e27670312b63fa
your name <your email>
2022-10-03 15:58:14 +0700

    config browser

--- features/step_definitions/demo.js
+++ features/step_definitions/demo.js
@@ -1,6 +1,19 @@
 const assert = require('assert');
-const { Given, When, Then } = require('cucumber');
-Given('Login page', async function () {
+const { test, expect, chromium } = require('@playwright/test');
+const { Given, When, Then, BeforeAll } = require('cucumber');
+BeforeAll(
+    (async () => {
+        const browser = await chromium.launch({ headless: false });
+        const context = await browser.newContext({
+            viewport: {
+              width: 1280, 
+              height: 720,
+            }} );
+        const page = await context.newPage();
+        page.goto('https://translate.google.com/?hl=vi&sl=en&tl=vi&op=translate');
+    })
+)
+Given('Login page into', function () {
     console.log("write code here");
 });
 
