fb09537d1e19802b8bfaf37387803399d76c44df
your name <your email>
2022-10-05 18:08:11 +0700

    set up browser

--- cucumber.js
+++ cucumber.js
@@ -2,5 +2,6 @@ module.exports = {
   default:[
     `--format-options '{"snippetInterface": "synchronous"}'`,
     '--require features/step_definitions/**/*.js', // Load step definitions
+    //'--require features/**/*.feature', 
   ].join(' ')
 }
--- features/step_definitions/demo.js
+++ features/step_definitions/demo.js
@@ -1,25 +1,19 @@
 const assert = require('assert');
+const { Given, When, Then } = require('@cucumber/cucumber');
 const { test, expect, chromium } = require('@playwright/test');
-const { Given, When, Then, BeforeAll } = require('cucumber');
-BeforeAll(
-    (async () => {
-        const browser = await chromium.launch({ headless: false });
-        const context = await browser.newContext({
-            viewport: {
-              width: 1280, 
-              height: 720,
-            }} );
-        const page = await context.newPage();
-        page.goto('https://translate.google.com/?hl=vi&sl=en&tl=vi&op=translate');
-    })
-)
-Given('Login page into', function () {
-    console.log("write code here");
+// const {getBeforeAll} = require ('./hook');
+//  getBeforeAll();
+
+Given('Login page', async function () {
+
+    await page.locator('#login_field').fill('1');
+      await page.locator('#password').fill('a');
+   await page.locator("input[type='submit']").click();
 });
 
-When('Input username and password', function () {
+When('Input {string} and {string}', function (string, string2) {
     console.log("write code here");
-});
+  });
 Then('Login successull', function () {
     console.log("write code here");
 });
